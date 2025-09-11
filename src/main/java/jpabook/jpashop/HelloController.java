f3558f5c30ae940b57980569bba1b80d8796ca59
sooin <lilesooin@gmail.com>
2025-09-11 16:24:13 -0400

    view 환경설정

---
+++ src/main/java/jpabook/jpashop/HelloController.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    // model: Controller에 데이터를 실어서 View에 넘길 수 있다
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!!");
+        return "hello"; // return: 화면이름. 관례상 templates에 hello.html 추가
+    }
+}
